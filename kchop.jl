46cfa76400c5bd9b027b67e67e747efc4bf1f1ec
timm <timm@ieee.org>
2024-12-15 21:26:56 -0500

    as

--- kchop.jl
+++ kchop.jl
@@ -26,15 +26,23 @@ Big=1E32
 @kwdef mutable struct Num   
   at=0; txt=""; n=0; mu=0; sd=0; md=20; lo=Big; hi= -Big; utopia = 1 end
    
-function NUM(s::String) 
-  Num(utopia = (s[end] == '-' ? 0 : 1)) end
+function NUM(s::String)::Num
+  Num(utopia = s[end] == '-' ? 0 : 1) end
 
-function rows(v::Vector, fun::Function) 
+function rows(v::Vector, fun::Function)::Vector
   [fun(x) for x in v] end
 
-function rows(file::String, fun::Function) 
+function rows(file::String, fun::Function)::nothing
   src = open(file)
   while ! eof(src)
     new = replace(readline(src), r"([ \t\n]|#.*)"=>"")
     if sizeof(new) != 0
       fun(map(what,split(new,","))) end end end
+
+function oo(obj,pre="")
+    println("\n$(typeof(obj)) {")
+    [println("  $f = $(getfield(obj, f))") for f in fieldnames(typeof(obj))]
+    println("}")
+end
+
+oo(NUM("aadas-"))
