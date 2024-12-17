d16c76ae78cf6190b4315e3014d3366efb15e9dd
timm <timm@ieee.org>
2024-12-17 18:35:03 -0500

    as

--- kchop.jl
+++ kchop.jl
@@ -1,3 +1,5 @@
+module kseed
+
 about="
 kseed.lua : multi-objective optimization via kmeans++ initialization.
 (c) 2024 Tim Menzies <timm@ieee.org>, MIT license.
@@ -16,37 +18,30 @@ Str,Fun = AbstractString,Function
 Atom    = Union{Symbol,Number,Char,Bool,Str}
 Big     = 1E32
 
-@kwdef mutable struct Num   
-  col=0; txt=""; utopia=1; n=0; mu=0; sd=0; md=20; lo= Big; hi= -Big end
-
-@kwdef mutable struct Sym   
-  col=0; txt=""; n=0; all=[]; mode=nothing; most=0 end
-
-@kwdef mutable struct Cols
-  all=[]; x=[]; y=[]; w=nothing end
-
-@kwdef mutable struct Data
-  rows=[]; cols=nothing end
+@kwdef mutable struct Data rows=[]; cols=nothing end
+@kwdef mutable struct Cols all=[]; x=[]; y=[]; w=nothing end
+@kwdef mutable struct Sym  pos=0; txt=""; n=0; all=[]; mode=nothing; most=0 end
+@kwdef mutable struct Num  pos=0; txt=""; n=0; goal=1; mu=0; sd=0; md=20; lo=Big; hi=-Big end
 
 #-------------------------------------------------------------------------------
 adds(i::Data, v::Vector) = [add(i,row) for row in v] 
-adds(i::Data, file::Str) = reads(open(file), (row) -> add(i,row)) 
+adds(i::Data, file::Str) = csv(open(file), (row) -> add(i,row)) 
 
-add(i::Cols, row::Vector)::Vector = [add(j, row[j.col]) for j in i.all] 
+add(i::Cols, row::Vector)::Vector = [add(j, row[j.pos]) for j in i.all] 
 add(i::Data, row::Vector) = isnothing(i.cols) ? i.cols = COLS(row) : push!(i.rows, add(i.cols,row)) 
 
 function add(i, x::Atom)::Atom
-  if x != "?"
+  if x != "?" 
     i.n  += 1
-    _add(i,x)
+    _add(i,x) end
   x end
-
+ 
 function _add(i::Num, x::Atom)
-  d     = value - i.mu
-  i.mu += d / i.n
-  i.sd += d * (value - i.mu)
-  i.lo  = min(i.lo, value)
-  i.hi  = max(i.hi, value) end
+  d     = x - i.mu
+  i.mu += d / i.n        # mu' = mu + (x-mu)/n
+  i.sd += d * (x - i.mu) # sd' = sd + (x-mu)(x-mu')
+  i.lo  = min(i.lo, x)
+  i.hi  = max(i.hi, x) end
 
 function _add(i::Sym, x::Atom) 
   tmp = i.has[x] = 1 + get(i.has, x, 0)
@@ -54,19 +49,18 @@ function _add(i::Sym, x::Atom)
     i.most,i.mode=tmp,x end end 
 
 #-------------------------------------------------------------------------------
-reads(src::IoStream, fun::Fun) =
-  while ! eof(src)
-    new = replace(readline(src), r"([ \t\n]|#.*)"=>"")
-    if sizeof(new) != 0
-      fun(map(coerce,split(new, ","))) end end
+csv(src::IOStream, fun::Fun) = while ! eof(src)
+                                 new = replace(readline(src), r"([ \t\n]|#.*)"=>"")
+                                 if sizeof(new) != 0
+                                   fun(map(coerce,split(new, ","))) end end
 
 oo(x)            = println(o(x)) 
 
 o(i::Atom)       = string(i)  
-o(i::Array)      = "["*join(map(o,i),", ")*"]" 
-o(i::NamedTuple) = "(" * join([":$f $(o( getfield(i,f)))" for f in keys(i)]," ")*")"
+o(i::Array)      = "[" * join(map(o,i),", ") * "]" 
+o(i::NamedTuple) = "(" * join(sort!([":$f $(o( getfield(i,f)))" for f in keys((i))])," ") * ")"
 o(i::Any)        = "$(typeof(i)){" * join([
-                   ":$f $( o( getfield(i,f)))" for f in fieldnames(typeof(i))]," ")*"}" 
+                     ":$f $( o( getfield(i,f)))" for f in fieldnames(typeof(i))]," ") * "}" 
 
 cli(nt::NamedTuple) = (;cli(Dict(pairs(nt)))...)
 cli(d::Dict) = begin
@@ -90,4 +84,6 @@ the = (;Dict(Symbol(k) => coerce(v)
 oo(Num(txt="fred-",mu=0.333333))
 oo([1,2,3,4])
 oo(1)
-oo(:a)
+oo(the) 
+
+end
